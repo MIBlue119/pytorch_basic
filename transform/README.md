196fdf39d2af2a4172f0a93e2da7e85cb2488a31
MIBlue119 <miblue119@gmail.com>
2022-07-09 09:50:33 +0800

    Feat: complete the stft part

--- transform/README.md
+++ transform/README.md
@@ -9,3 +9,8 @@
 - https://github.com/echocatzh/MTFAA-Net/blob/main/stft.py
 - https://espnet.github.io/espnet/_modules/espnet2/layers/stft.html
 - OpenUnmix: https://github.com/sigsep/open-unmix-pytorch/blob/7623406990e68bd60737bbb70680298e02bb27d0/openunmix/transforms.py#L58
+
+- Pytorch Module & Parameters
+    - https://medium.com/%E6%88%91%E5%B0%B1%E5%95%8F%E4%B8%80%E5%8F%A5-%E6%80%8E%E9%BA%BC%E5%AF%AB/pytorch-module-parameters-%E4%BD%BF%E7%94%A8-a3393ca391ee
+    - https://blog.csdn.net/qq_43391414/article/details/120484239
+        - What is the difference to set the tensor and package with `nn.Parameter`? 
--- transform/stft.py
+++ transform/stft.py
@@ -1,24 +1,92 @@
 """Define  STFT/ ISTFT class."""
+from importlib.metadata import requires
 from typing import Optional, Tuple, Union
 import torch 
-from torch import nn.Module 
+from torch import Tensor 
+import torch.nn as nn 
 
 class STFT(nn.Module):
     def __init__(
         self, 
-        window: Optional[str] = 'hann', 
-        fft_size: int = 512,         
-        window_size: int = None, 
-        hop_size: int = 128
+        n_fft: int = 512,         
+        win_length: int = None, 
+        hop_length: int = 128,
+        window: Optional[nn.Parameter] = None,
+        center: bool = True
     ):
         super(STFT, self).__init__()
         self.window = window 
-        self.fft_size = fft_size
-        if window_size is None:
-            self.window_size = fft_size
+        self.n_fft = n_fft
+        if win_length is None:
+            self.win_length = n_fft
         else:
-            self.window_size = window_size 
-        self.hop_size = hop_size
-    
+            self.win_length = win_length 
+        self.hop_length = hop_length
 
-    def forward(self, x):
+        if window is None: 
+            self.window = nn.Parameter(
+                # Defualt window is a hann window: https://pytorch.org/docs/stable/generated/torch.hann_window.html
+                torch.hann_window(
+                    window_length = self.win_length,
+                    requires_grad = False
+                )
+            )
+        else:
+            self.window = window
+
+        # Define whether to pad input on both sides so that the ttt-th frame is centered at time 
+        self.center = center
+
+    def forward(self, x: Tensor):
+        """STFT forward path
+        Args:
+            x (Tensor): audio waveform of shape (batch_size, n_frames, n_channels)
+        
+        Returns:
+            output: (batch_size, n_channels,n_freq,n_frames)
+        """    
+        # Extract the shape of the input signal
+        shape = x.size()
+
+        # Unwrap the shape 
+        nb_batches, nb_channels, nb_samples = shape 
+
+        # Use torch.view to reshape the input signal to pack the batch dimension
+        # Ref: https://pytorch.org/docs/stable/generated/torch.Tensor.view.html
+        # view(-1) mean the shape would be inferred from the shape of the input
+        # Example: 
+        # x = torch.randn(1, 2, 16000)
+        # shape = x.shape
+        # x.view(-1, shape[-1]) -> shape size = (2, 16000)  
+        x = x.view(-1, shape[-1])
+
+        # torch.stft doc: https://pytorch.org/docs/stable/generated/torch.stft.html
+        # Input must be either a 1-D time sequence or a 2-D batch of time sequences.
+        
+        # shape of input x: (batch_size, n_samples)
+        comlex_stft = torch.stft(
+            input = x, 
+            n_fft = self.n_fft, 
+            hop_length = self.hop_length,
+            window = self.window,
+            center = self.center,
+            normalized = False,
+            onesided = True,    #  Controls whether to return half of results to avoid redundancy for real inputs.
+            pad_mode = "reflect",
+            return_complex = True
+        )
+        # shape of complex_stft: (batch_size, n_freq, n_frames)
+        # Use `torch.view_as_real` to convert the complex tensor to real tensor
+        # where the last dimension of size 2 represents the real and imaginary components of complex numbers.
+        # https://pytorch.org/docs/stable/generated/torch.view_as_real.html
+        real_tensor_stft = torch.view_as_real(comlex_stft)
+        # real_tensor_stft shape: (batch_size, n_freq, n_frames, 2=real+imag)
+
+        # Unpack the batch 
+        # Combine the shape : torch.size(nb_batches, nb_channels)+ torch.size(n_freq, n_frames, 2=real+imag)
+        output = output.view(shape[:-1], real_tensor_stft.shape[-3:])
+        # shape of output: (nb_batches, nb_channels, n_freq, n_frames, 2=real+imag)
+
+        return output
+
+
