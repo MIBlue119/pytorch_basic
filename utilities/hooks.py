f5b8fb1d59bcd36046c821a4aff3f4c218ab4a0c
MIBlue119 <miblue119@gmail.com>
2022-07-09 10:52:21 +0800

    Feat: add hooks funciton

---
+++ utilities/hooks.py
@@ -0,0 +1,28 @@
+"""
+Some hook modules for torch nn.Module
+
+Ref:
+- How to use pytorch hooks: https://medium.com/the-dl/how-to-use-pytorch-hooks-5041d777f904
+"""
+import torch.nn as nn
+
+
+
+def hook_show_module_parameters(module: nn.Module, input, output):
+    """Ref:https://blog.paperspace.com/pytorch-hooks-gradient-clipping-debugging/"""
+    print(module)
+    print("---------------Module Parameters---------------")
+    neles = sum(  [ param.nelement() for param in module.parameters()])
+    print(neles)
+    print("---------------Module input Gradients---------------")
+    for in_ in input:
+        try:
+            print(f"{in_shape:{in_.shape}}")
+        except AttributeError:
+            print("None found for Gradient")
+    print("---------------Module Outputs---------------")
+    for out in output:
+        try:
+            print(f"out shape: {out.shape}")
+        except AttributeError:
+            print("None found for Output")
